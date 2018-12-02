88a6ab6710599d2806cbcecb31a5d07c59fb2639
Lucita31 <45539107+Lucita31@users.noreply.github.com>
2018-12-02 11:59:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,42 @@ app.get('/', (req, res) => {
 });
 
 // scraper endpoint
+app.get('/dadjokes/:keyword', (req, res) => {
+  var keyword = req.params.keyword;
 
+  function findDadJokeImage(keyword) {
+    var nightmare = Nightmare({show: true});
+
+    return nightmare
+      .goto('https://www.google.com')
+      .insert('input[title="Search"]', `Dad Joke ${keyword}`)
+      .click('input[value="Google Search"]')
+      .wait('a.q.qs')
+      .click('a.q.qs')
+      .wait('div#res.med')
+      .evaluate(function() {
+        var photoDivs = document.querySelectorAll('img.rg_ic');
+        var list = [].slice.call(photoDivs);
+
+        return list.map(function(div) {
+          return div.src;
+        });
+      })
+      .end()
+      .then(function(result) {
+        return result.slice(1, 10);
+      })
+      .then(function(images) {
+        res.json(images);
+      })
+      .catch(function(error) {
+        Console.error('Search failed',error);
+      })
+  }
+
+   findDadJokeImage(keyword);
+
+});
 
 app.listen(port, () => {
   console.log(`app running on ${port}`);
